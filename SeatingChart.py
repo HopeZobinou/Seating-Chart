f76c063357729122767545e890dbfeaf1440053d
HopeZobinou <81893993+HopeZobinou@users.noreply.github.com>
2021-04-08 12:16:00 -0400

    Update SeatingChart.py

--- SeatingChart.py
+++ SeatingChart.py
@@ -8,15 +8,16 @@ Date: 4/7/21
 """
 
 import random
-Students = [] #The list that holds the students names
-NewStudentOrder = [] #The list that holds the students names in a random order
-SeatNum = 1 #Seat number
-NumOfStudents = int(input("How many students do you have: "))
-for i in range(NumOfStudents): #For loop that gets the student's names and appends them to the "Students" list
-    StudentName = input("Enter student's name: ")
-    Students.append(StudentName) 
-print()
-NewStudentOrder = random.sample(Students, NumOfStudents) #Random sample makes a list from the "Students" list in random order w/o replacement
-for i in range(NumOfStudents): #For loop that prints the new seating chart
-    print("Student Seat #:", SeatNum, "Student Name:", NewStudentOrder[i])
-    SeatNum += 1
+if __name__ == '__main__':
+    Students = [] #The list that holds the students names
+    NewStudentOrder = [] #The list that holds the students names in a random order
+    SeatNum = 1 #Seat number
+    NumOfStudents = int(input("How many students do you have: "))
+    for i in range(NumOfStudents): #For loop that gets the student's names and appends them to the "Students" list
+        StudentName = input("Enter student's name: ")
+        Students.append(StudentName) 
+    print()
+    NewStudentOrder = random.sample(Students, NumOfStudents) #Random sample makes a list from the "Students" list in random order w/o replacement
+    for i in range(NumOfStudents): #For loop that prints the new seating chart
+        print("Student Seat #:", SeatNum, "Student Name:", NewStudentOrder[i])
+        SeatNum += 1
